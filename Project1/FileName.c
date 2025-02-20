78912af14ef7d0bf202e1b5f967c5f829a5f9bc7
DJN-1 <ujin7294@gmail.com>
2025-02-20 09:43:17 +0900

    프라임넘버 찾기 소스

---
+++ Project1/FileName.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int column = 0;
+	int a;
+	int i, j;
+
+	printf("2 이상의 정수를 입력하세요 : ");
+	scanf("%d", &a);
+
+	for (i = 2; i <= a; i++) {
+		for (j = 2; j < i; j++) {
+			if (i % j == 0) break;
+		}
+		if(j == i) {
+			printf("%5d", i);
+			column++;
+			//5개마다 줄바꿈 처리
+			if (column == 5) {
+				column = 0;
+				printf("\n");
+			}
+		}
+	}
+}
